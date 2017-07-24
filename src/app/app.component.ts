3fc7180855f651a4a0587b91b5019bbe24b5c15e
Justin Chang <jqchang@gmail.com>
2017-07-23 19:36:04 -0700

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  msgs = [
+    {
+        email:'BILL@gates.com',
+        importance:true,
+        subject:'New Windows',
+        content:'Windows XI will launch in year 2100.'
+    },
+    {
+      email:'ada@lovelace.com',
+      importance:true,
+      subject:'Programming',
+      content:'Enchantress of Numbers'
+    },
+    {
+      email:'john@carmac.com',
+      importance:false,
+      subject:'Updated Algo',
+      content:'New algorithm for shadow volumes.'
+    },
+    {
+      email:'gabe@newel.com',
+      importance:false,
+      subject:'HL3!',
+      content:'Just kidding...'
+    },
+  ]
 }
